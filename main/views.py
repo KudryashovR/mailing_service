e133c38ce4dfd48c91281a95cce2efba50791a70
KudryashovR <romkud55@gmail.com>
2024-06-24 23:32:44 +0300

    ADD: CRUD views for model Mailing

--- main/views.py
+++ main/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Mailing
+from .forms import MailingForm
 
 
-def index(request):
-    return render(request, 'main/index.html')
+# def index(request):
+#     return render(request, 'main/index.html')
+
+
+class MailingListView(ListView):
+    model = Mailing
+    template_name = 'main/mailing_list.html'
+    context_object_name = 'mailings'
+
+
+class MailingDetailView(DetailView):
+    model = Mailing
+    template_name = 'main/mailing_detail.html'
+    context_object_name = 'mailing'
+
+
+class MailingCreateView(CreateView):
+    model = Mailing
+    template_name = 'main/mailing_form.html'
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing_list')
+
+
+class MailingUpdateView(UpdateView):
+    model = Mailing
+    template_name = 'main/mailing_form.html'
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing_list')
+
+    def get_success_url(self):
+        return reverse_lazy('mailing_detail', kwargs={'pk': self.object.pk})
+
+
+class MailingDeleteView(DeleteView):
+    model = Mailing
+    template_name = 'main/mailing_confirm_delete.html'
+    success_url = reverse_lazy('mailing_list')

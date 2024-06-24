115860717da6d3d298b14ba01c09993f56422d0d
KudryashovR <romkud55@gmail.com>
2024-06-24 23:58:59 +0300

    ADD: MailingListView

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Mailing, Client
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'message', 'status', 'scheduled_time')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'last_name', 'first_name', 'second_name', 'comment')
+
--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index
+from main.views import MailingListView, MailingDetailView, MailingCreateView, MailingUpdateView, MailingDeleteView
 
 app_name = MainConfig.name
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', MailingListView.as_view(), name='home'),
 ]
